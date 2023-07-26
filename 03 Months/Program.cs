7b82fca64c86324188651f4078adba9c7de4e862
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-07-26 21:42:49 +0200

    9:42

--- 03 Months/Program.cs
+++ 03 Months/Program.cs
@@ -4,49 +4,49 @@
     {
         static void Main(string[] args)
         {
-            int num = int.Parse(Console.ReadLine());
-            switch (num)
+            int monthNum = int.Parse(Console.ReadLine());
+            switch (monthNum)
             {
                 case 1:
-                    Console.WriteLine("January");
-                    break;
-                    case 2:
-                    Console.WriteLine("Faburary");
-                    break;
-                    case 3:
-                    Console.WriteLine("March");
-                    break;
-                    case 4:
-                    Console.WriteLine("April");
-                    break;
-                    case 5:
-                    Console.WriteLine("May");
-                    break;
-                    case 6:
-                    Console.WriteLine("June");
-                    break;
-                    case 7:
-                    Console.WriteLine("July");
-                    break;
-                    case 8:
-                    Console.WriteLine("August");
-                    break;
-                    case 9:
-                    Console.WriteLine("September");
-                    break;
-                    case 10:
-                    Console.WriteLine("Öct");
-                    break;
-                    case 11:
-                    Console.WriteLine("Nov");
-                    break;
-                    case 12:
-                    Console.WriteLine("Dec");
-                    break;
+                Console.WriteLine("January");
+                break;
+                case 2:
+                Console.WriteLine("Faburary");
+                break;
+                case 3:
+                Console.WriteLine("March");
+                break;
+                case 4:
+                Console.WriteLine("April");
+                break;
+                case 5:
+                Console.WriteLine("May");
+                break;
+                case 6:
+                Console.WriteLine("June");
+                break;
+                case 7:
+                Console.WriteLine("July");
+                break;
+                case 8:
+                Console.WriteLine("August");
+                break;
+                case 9:
+                Console.WriteLine("September");
+                break;
+                case 10:
+                Console.WriteLine("Öct");
+                break;
+                case 11:
+                Console.WriteLine("Nov");
+                break;
+                case 12:
+                Console.WriteLine("Dec");
+                break;
 
-                    default: 
-                    Console.WriteLine("crazy input");
-                    break;
+                default: 
+                Console.WriteLine("crazy input");
+                break;
                     
             }
         }
---
+++ 03 Order/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _03_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            string orderNumber = string.Empty;
+            if (num1 > num2 && num1>num3)
+            {
+                orderNumber = num1.ToString();
+                if (num2>num3)
+                {
+                    orderNumber += " "+num2.ToString()+" " + num3.ToString();
+                    Console.WriteLine(orderNumber);
+                }
+                else
+                {
+                    orderNumber += " " + num3.ToString() + " " + num2.ToString();
+                    Console.WriteLine(orderNumber);
+                }
+            }
+            else if(num2 > num3 && num2>num1)
+            {
+                orderNumber = num2.ToString();
+                if (num1 > num3)
+                {
+                    orderNumber += " " + num1.ToString() + " " + num3.ToString();
+                    Console.WriteLine(orderNumber);
+                }
+                else
+                {
+                    orderNumber += " " + num3.ToString() + " " + num1.ToString();
+                    Console.WriteLine(orderNumber);
+                }
+            }
+            else if (num3 > num2 && num3 > num1)
+            {
+                orderNumber = num3.ToString();
+                if (num2 > num1)
+                {
+                    orderNumber += " " + num2.ToString() + " " + num1.ToString();
+                    Console.WriteLine(orderNumber);
+                }
+                else
+                {
+                    orderNumber += " " + num1.ToString() + " " + num2.ToString();
+                    Console.WriteLine(orderNumber);
+                }
+            }
+        }
+    }
+}
