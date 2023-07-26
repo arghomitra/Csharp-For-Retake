643f87b1d504d0c47550040a53971d51a6dfcdb6
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-07-26 19:45:10 +0200

    7:45

---
+++ 02 Odd/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integerValue = int.Parse(Console.ReadLine());
+            if (integerValue/2 != 0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
