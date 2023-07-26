d1f199252d8ce0f3f23a1405a341a15dde750b9f
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-07-26 19:27:24 +0200

    26/07/23

---
+++ 02 ASCII/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02_ASCII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = int.Parse(Console.ReadLine());//The number is read from the console and converted to an integer using int.Parse().
+            char ascii= Convert.ToChar(userInput);
+
+            Console.WriteLine(ascii.ToString().ToLower());
+            Console.WriteLine(ascii.ToString().ToUpper());
+            
+        }
+    }
+}
