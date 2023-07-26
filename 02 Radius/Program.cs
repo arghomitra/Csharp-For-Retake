5150928291dd7ac715e19e000c147f58c796ce85
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-07-26 19:54:53 +0200

    7:54

---
+++ 02 Radius/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02_Radius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area =Convert.ToDouble(Console.ReadLine()) ;
+            //r = √(A / π).
+            double pi = Math.PI;
+            double r = Math.Sqrt(area/pi);
+            double result = Math.Round(r, 2);
+            Console.WriteLine(result.ToString("N2"));
+        }
+    }
+}
