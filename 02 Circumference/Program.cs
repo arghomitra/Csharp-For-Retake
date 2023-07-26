0b11d9f7ae03c14871fe53bdad6f26cc5f35d7db
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-07-26 19:36:48 +0200

    7:36

---
+++ 02 Circumference/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02_Circumference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           decimal l = Convert.ToDecimal(Console.ReadLine());
+           decimal w = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(2 * (l + w));
+        }
+    }
+}
