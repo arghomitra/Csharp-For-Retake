7c627aeb30a9722167bf9d07e89d89e08681c946
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-07-26 19:24:58 +0200

    new
    new

---
+++ 02 Decimal part/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02_Decimal_part
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal decimalNum = Convert.ToDecimal(Console.ReadLine()); //10.78001
+
+            int integerValue = (int)Math.Floor(decimalNum);//10
+            
+            decimal result = decimalNum-integerValue;//0.78001
+
+            Console.WriteLine(result.ToString("N6"));//0.780010
+        }
+    }
+}
