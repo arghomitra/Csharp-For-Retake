b8a4f96ed50961a3a3fbee6caa1dc83e7c85c022
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-07-26 20:37:53 +0200

    8:37

---
+++ 03 Even/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _03_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            if (a % 2 == 0)
+            {
+                if (b % 2 == 0)
+                {
+                    if ((c % 2) == 0)
+                    {
+                        sum = a + b + c;
+                        Console.WriteLine(sum);
+                    }
+                }
+                else
+                {
+                    if ((c % 2) == 0)
+                    {
+                        sum = a  + c;
+                        Console.WriteLine(sum);
+                    }
+                    else
+                    {
+                        sum = a;
+                        Console.WriteLine(sum);
+                    }
+                }
+
+                
+
+            }
+            else if (b % 2 == 0)
+            {
+                if ((c % 2) == 0)
+                {
+                    sum = b + c;
+                    Console.WriteLine(sum);
+                }
+                else
+                {
+                Console.WriteLine(b);
+                }
+                
+            }
+            else
+            {
+                sum = c;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
