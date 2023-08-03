5471077d09f14dc68e5bb714d3378b603e848617
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-08-03 20:49:54 +0200

    xc

---
+++ 03 Curzon/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03_Curzon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int number;
+            bool success = int.TryParse(num, out number);
+            if (success || number>0)
+            {
+                int power = (int)Math.Pow(2, number) + 1;
+                int devider = 2 * number + 1;
+                if (power % devider ==0) 
+                {
+                    Console.WriteLine("T");
+                }
+                else
+                {
+                    Console.WriteLine("F");
+                }
+            }
+            else
+            {
+                Console.WriteLine("crazy input");
+            }
+
+        }
+    }
+}
---
+++ 03 Special/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_Special
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          char userInput = Convert.ToChar(Console.ReadLine().ToUpper());
+          int userInputInt = (int)userInput;
+          if ((userInputInt>=33 && userInputInt<=47)|| (userInputInt >= 58 && userInputInt <= 64) || (userInputInt >= 91 && userInputInt <= 96) || (userInputInt >= 123 && userInputInt <= 126))
+            {
+                Console.WriteLine("Special charecter");
+            }
+          else if(userInputInt >= 48 && userInputInt <= 57)
+            {
+                Console.WriteLine("Degit");
+            }
+          else if (userInputInt >= 48 && userInputInt <= 57)
+            {
+            Console.WriteLine("Degit");
+            }
+        }
+    }
+}
