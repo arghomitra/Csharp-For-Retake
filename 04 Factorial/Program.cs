3f5997241b114f8d03cf5fe3c8b2b81722e61f4f
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-08-09 14:05:04 +0200

    factorial

---
+++ 04 Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace _04_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int number;
+            bool success = int.TryParse(input, out number);
+            if (success && number >0)
+            {
+                int factorial = 1;
+                for (int i = 1;i<=number;i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine(factorial);
+
+            }
+            else
+            {
+                Console.WriteLine( "Crezy Input!");
+            }
+        }
+    }
+}
