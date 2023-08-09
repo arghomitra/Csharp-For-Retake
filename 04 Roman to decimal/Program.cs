ff3f5990106b375815d74a90ee9e189e16db4b9f
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-08-09 17:48:11 +0200

    kw;rlww

---
+++ 04 Roman to decimal/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _04_Roman_to_decimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string roman = Console.ReadLine();
+            for (int i = 0;i< roman.Length;i++)
+            {
+                switch (roman[i])
+                {
+                    case 'D':
+                    Console.WriteLine(500);
+                    break;
+                    case 'M':
+                        Console.WriteLine(1000);
+                        break;
+                    case 'C':
+                        Console.WriteLine(100);
+                        break;
+                    case 'L':
+                        Console.WriteLine(50);
+                        break;
+                    case 'X':
+                        Console.WriteLine(10);
+                        break;
+                    case 'V':
+                        Console.WriteLine(5);
+                        break;
+                    case 'I':
+                        Console.WriteLine(1);
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
