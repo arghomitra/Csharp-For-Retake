e0c449375364a5dd18005bd608deb5096d1044bf
arghomitra <117085618+arghomitra@users.noreply.github.com>
2023-08-09 15:01:37 +0200

    SDKJF

---
+++ 04 smallest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04_smallest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallest = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "x")
+                {
+                    break;
+                }
+
+                int number;
+                bool success = int.TryParse(input, out number);
+
+                if (success)
+                {
+                    if (number < smallest)
+                    {
+                        smallest = number;
+                    }
+                    else if (smallest == 0)
+                    {
+                        smallest = number;
+                    }
+                }
+            }
+
+            Console.WriteLine(smallest);
+        }
+    }
+}
